77249a673f291b0438b8eb3c52332ebb7b815f0d
owinogabriel <g73346920@gmail.com>
2025-07-31 01:55:15 +0300

    feat(login): add user login form with Zod validation

--- frontend/app/auth/sign.tsx
+++ frontend/app/auth/sign.tsx
@@ -4,6 +4,10 @@ import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import type { NavigationProp } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginFormInputs, loginSchema } from "@/schema/authSchema";
+
 
 type RootStackParamList = {
   sign: undefined;
@@ -14,9 +18,27 @@ type NavigationProps = NavigationProp<RootStackParamList>;
 
 export default function Index() {
   const navigation = useNavigation<NavigationProps>();
-  const [selectedRole, setSelectedRole] = useState<string | null>(null);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      role: undefined,
+    },
+  });
 
   const roles = ["Admin", "Teacher", "Student"];
+
+  const onSubmit = (data: LoginFormInputs) => {
+    console.log("Form Data:", data);
+  };
 
   return (
     <SafeAreaProvider className="">
@@ -47,28 +69,63 @@ export default function Index() {
           {/* Email */}
           <View>
             <Text className="text-lg text-[#2C3E50] mb-2">Email</Text>
-            <TextInput
-              keyboardType="email-address"
-              className="border border-[#1ABC9C] rounded-lg h-12 px-2.5"
-              placeholder="Enter your Email"
-              placeholderTextColor="#7E7B7B"
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  keyboardType="email-address"
+                  className="border border-[#1ABC9C] rounded-lg h-12 px-2.5"
+                  placeholder="Enter your Email"
+                   placeholderTextColor="text-[#2C3E50]"
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  autoCapitalize="none"
+                />
+              )}
             />
+            {errors.email && (
+              <Text className="text-red-500 text-sm mt-1">
+                {errors.email.message}
+              </Text>
+            )}
           </View>
+
           {/* Password */}
           <View className="mt-5">
             <Text className="text-lg text-[#2C3E50] mb-2">Password</Text>
             <View className="flex-row border border-[#1ABC9C] items-center h-12 rounded-lg px-2.5">
-              <TextInput
-                className="flex-1"
-                placeholder="Enter your password"
-                secureTextEntry={true}
-                placeholderTextColor="#7E7B7B"
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    className="flex-1"
+                    placeholder="Enter your password"
+                    secureTextEntry={!showPassword}
+                    placeholderTextColor="text-[#2C3E50]"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
               />
-              <TouchableOpacity>
-                <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <FontAwesome
+                  name={showPassword ? "eye" : "eye-slash"}
+                  size={24}
+                  color="#7E7B7B"
+                />
               </TouchableOpacity>
             </View>
+            {errors.password && (
+              <Text className="text-red-500 text-sm mt-1">
+                {errors.password.message}
+              </Text>
+            )}
           </View>
+
           {/* Forgot Password */}
           <TouchableOpacity>
             <Text className="text-lg mt-2 text-right text-[#34967C]">
@@ -79,32 +136,46 @@ export default function Index() {
           {/* Role Selection */}
           <View className="mt-5">
             <Text className="text-lg text-[#2C3E50] mb-2">Sign in as:</Text>
-            <View className="flex-row justify-around">
-              {roles.map((role) => (
-                <TouchableOpacity
-                  key={role}
-                  className={`py-2 px-4 rounded-full ${
-                    selectedRole === role
-                      ? "bg-[#1ABC9C]"
-                      : "border border-[#1ABC9C]"
-                  }`}
-                  onPress={() => setSelectedRole(role)}
-                >
-                  <Text
-                    className={`${
-                      selectedRole === role ? "text-white" : "text-[#1ABC9C]"
-                    } font-semibold`}
-                  >
-                    {role}
-                  </Text>
-                </TouchableOpacity>
-              ))}
-            </View>
+            <Controller
+              control={control}
+              name="role"
+              render={({ field: { onChange, value } }) => (
+                <View className="flex-row justify-around">
+                  {roles.map((role) => (
+                    <TouchableOpacity
+                      key={role}
+                      className={`py-2 px-4 rounded-full ${
+                        value === role
+                          ? "bg-[#1ABC9C]"
+                          : "border border-[#1ABC9C]"
+                      }`}
+                      onPress={() => onChange(role)}
+                    >
+                      <Text
+                        className={`${
+                          value === role ? "text-white" : "text-[#1ABC9C]"
+                        } font-semibold`}
+                      >
+                        {role}
+                      </Text>
+                    </TouchableOpacity>
+                  ))}
+                </View>
+              )}
+            />
+            {errors.role && (
+              <Text className="text-red-500 text-sm mt-1 text-center">
+                {errors.role.message}
+              </Text>
+            )}
           </View>
         </View>
 
         <View className="px-5">
-          <TouchableOpacity className="bg-[#2B876E] h-[53px] rounded-lg mt-6 flex justify-center items-center shadow-md">
+          <TouchableOpacity
+            className="bg-[#2B876E] h-[53px] rounded-lg mt-6 flex justify-center items-center shadow-md"
+            onPress={handleSubmit(onSubmit)}
+          >
             <Text className="text-lg text-white font-semibold">Sign In</Text>
           </TouchableOpacity>
         </View>
