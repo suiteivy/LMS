304e9aa1da433881143da0f848661290b334bd37
owinogabriel <g73346920@gmail.com>
2025-07-31 01:55:29 +0300

    feat(signup): create user registration form with Zod.

--- frontend/app/auth/signUp.tsx
+++ frontend/app/auth/signUp.tsx
@@ -1,19 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import type { NavigationProp } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { SignUpFormInputs, signUpSchema } from "@/schema/authSchema";
 
-// Defined route params
+// Defined navigation route params
 type RootStackParamList = {
   "auth/sign": undefined;
+  "auth/signUp": undefined
 };
 
 type NavigationProps = NavigationProp<RootStackParamList>;
 
 export default function App() {
   const navigation = useNavigation<NavigationProps>();
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  // Initialized React Hook Form with Zod resolver
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignUpFormInputs>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  // Function to handle form submission
+  const onSubmit = (data: SignUpFormInputs) => {
+    console.log("Form Data:", data);
+    alert("Account created successfully! Please log in.");
+    navigation.navigate("auth/sign");
+  };
 
   return (
     // Entire app wrapped inside SafeAreaProvider and SafeAreaView to prevent UI overlap with device notches.
@@ -38,28 +65,64 @@ export default function App() {
             {/* Email */}
             <View>
               <Text className="text-lg text-[#2C3E50] mb-2">Email</Text>
-              <TextInput
-                keyboardType="email-address"
-                className="border border-[#1ABC9C] rounded-lg h-12 px-2.5 w-full focus:border-[#2B876E] focus:ring-2 focus:ring-[#2B876E]"
-                placeholder="Enter your email"
-                placeholderTextColor="#7E7B7B"
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <TextInput
+                    keyboardType="email-address"
+                    className="border border-[#1ABC9C] rounded-lg h-12 px-2.5 w-full focus:border-[#2B876E] focus:ring-2 focus:ring-[#2B876E]"
+                    placeholder="Enter your email"
+                    placeholderTextColor="text-[#2C3E50]"
+                    onBlur={onBlur}
+                    onChangeText={onChange}
+                    value={value}
+                    autoCapitalize="none"
+                  />
+                )}
               />
+              {errors.email && (
+                <Text className="text-red-500 text-sm mt-1">
+                  {errors.email.message}
+                </Text>
+              )}
             </View>
 
             {/* Password */}
             <View>
               <Text className="text-lg text-[#2C3E50] mb-2">Password</Text>
               <View className="flex-row items-center border border-[#1ABC9C] h-12 rounded-lg px-2.5 relative">
-                <TextInput
-                  secureTextEntry={true}
-                  className="flex-1 text-[#2C3E50]"
-                  placeholder="Enter your password"
-                  placeholderTextColor="#7E7B7B"
+                <Controller
+                  control={control}
+                  name="password"
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      secureTextEntry={!showPassword}
+                      className="flex-1 text-[#2C3E50]"
+                      placeholder="Enter your password"
+                      placeholderTextColor="text-[#2C3E50]"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
                 />
-                <TouchableOpacity className="absolute right-3 p-1">
-                  <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+                <TouchableOpacity
+                  className="absolute right-3 p-1"
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <FontAwesome
+                    name={showPassword ? "eye" : "eye-slash"}
+                    size={24}
+                    color="#7E7B7B"
+                  />
                 </TouchableOpacity>
               </View>
+              {errors.password && (
+                <Text className="text-red-500 text-sm mt-1">
+                  {errors.password.message}
+                </Text>
+              )}
             </View>
 
             {/* Confirm Password */}
@@ -68,21 +131,45 @@ export default function App() {
                 Confirm Password
               </Text>
               <View className="flex-row items-center border border-[#1ABC9C] h-12 rounded-lg px-2.5 relative">
-                <TextInput
-                  secureTextEntry={true}
-                  className="flex-1 text-[#2C3E50]"
-                  placeholder="Confirm your password"
-                  placeholderTextColor="#7E7B7B"
+                <Controller
+                  control={control}
+                  name="confirmPassword"
+                  render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                      secureTextEntry={!showConfirmPassword}
+                      className="flex-1 text-[#2C3E50]"
+                      placeholder="Confirm your password"
+                      placeholderTextColor="text-[#2C3E50]"
+                      onBlur={onBlur}
+                      onChangeText={onChange}
+                      value={value}
+                    />
+                  )}
                 />
-                <TouchableOpacity className="absolute right-3 p-1">
-                  <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+                <TouchableOpacity
+                  className="absolute right-3 p-1"
+                  onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  <FontAwesome
+                    name={showConfirmPassword ? "eye" : "eye-slash"}
+                    size={24}
+                    color="#7E7B7B"
+                  />
                 </TouchableOpacity>
               </View>
+              {errors.confirmPassword && (
+                <Text className="text-red-500 text-sm mt-1">
+                  {errors.confirmPassword.message}
+                </Text>
+              )}
             </View>
           </View>
 
           {/* Create Account Button */}
-          <TouchableOpacity className="bg-[#2B876E] p-5 h-[53px] rounded-lg mt-6 flex justify-center items-center w-full shadow-md">
+          <TouchableOpacity
+            className="bg-[#2B876E] p-5 h-[53px] rounded-lg mt-6 flex justify-center items-center w-full shadow-md"
+            onPress={handleSubmit(onSubmit)}
+          >
             <Text className="text-lg text-white font-semibold">
               Create Account
             </Text>
