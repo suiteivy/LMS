36d2254918c690c12ef2e313fa3aea57e32cccd1
owinogabriel <g73346920@gmail.com>
2025-07-30 23:40:51 +0300

    feat:    // Entire app wrapped inside SafeAreaProvider and SafeAreaView to prevent UI overlap with device notches.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,17 +1,61 @@
-import "../styles/global.css"
-import { Text, View } from "react-native";
-import MSPrimeForm from "./mobile/src/components/MSPrimeForm";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import React from "react";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 
+type RootStackParamList = {
+  "auth/sign": undefined;
+ "auth/signUp": undefined;
+};
+
+type NavigationProps = NavigationProp<RootStackParamList>;
 export default function Index() {
+  const navigation = useNavigation<NavigationProps>();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-     <Text className="text-red-700">Hello Devs</Text>
-    </View>
+    // Entire app wrapped inside SafeAreaProvider and SafeAreaView to prevent UI overlap with device notches.
+    <SafeAreaProvider>
+      <SafeAreaView className="flex-1">
+        <View className="flex-1 bg-[#F1FFF8]">
+          {/* Company Logo */}
+          <View className="w-full items-center p-5 mb-12">
+            <Image source={require("@/assets/images/react-logo.png")} />
+          </View>
+
+          {/* Hero Section */}
+          <View className="items-center">
+            <Text className="text-[#2C3E50] font-extrabold text-4xl text-center mb-3">
+              Welcome to Our Learning Hub
+            </Text>
+            <Text className="text-[#2C3E50] text-lg font-extralight text-center">
+              Unlock your full potential with our comprehensive courses.
+            </Text>
+          </View>
+
+          {/* Button section */}
+          <View className="absolute bottom-0 w-full mb-6">
+            <View className="flex-row gap-5 px-5">
+              <TouchableOpacity
+                className="rounded-full py-4 px-1 items-center bg-[#128C7E] flex-1"
+                onPress={() => navigation.navigate("auth/signUp")}
+              >
+                <Text className="text-white text-lg font-medium">
+                  Join here
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                className="border-2  border-[#128C7E] rounded-full py-4 px-1 items-center flex-1"
+                onPress={() => navigation.navigate("auth/sign")}
+              >
+                <Text className="text-[#2C3E50] text-lg font-medium">
+                  Sign In
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
