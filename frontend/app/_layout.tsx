bfea8746589cb4a4bcff947e7973968830ac5ab6
owinogabriel <g73346920@gmail.com>
2025-07-30 14:53:14 +0300

    modified

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,5 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return <Stack 
   screenOptions={{
-      headerShown: false,}}/>;
+      headerShown: false,
+    }}
+  />;
 }
