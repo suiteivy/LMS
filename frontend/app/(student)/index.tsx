5768bada5cfd8dc3235fbafa316728029f545726
chitsaka <luckykatana73@gmail.com>
2025-08-01 08:49:11 +0300

    modified index

--- frontend/app/(student)/index.tsx
+++ frontend/app/(student)/index.tsx
@@ -1,6 +1,5 @@
 import { Text, View,  TouchableOpacity, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useState } from "react";
 import CircularProgress from "../../components/CircularProgress";
 
 
