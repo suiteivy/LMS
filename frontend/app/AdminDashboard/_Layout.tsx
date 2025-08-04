2e2ab52502f40d2f7f105ee3f3e8de86a74796e7
owinogabriel <g73346920@gmail.com>
2025-08-04 10:52:43 +0300

    core:modifed

---
+++ frontend/app/AdminDashboard/_Layout.tsx
@@ -0,0 +1,16 @@
+// // app/admin/_layout.tsx
+
+// import { View } from "react-native";
+// import Slot from "expo-router";
+// import { SidebarNavigator } from "./SidebarNavigator";
+
+// export default function AdminLayout() {
+//   return (
+//     <View className="flex-1 flex-row">
+//       <SidebarNavigator />
+//       <View className="flex-1">
+//         <Slot />
+//       </View>
+//     </View>
+//   );
+
