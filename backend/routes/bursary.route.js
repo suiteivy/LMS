2297d0a578e292e9e15b43677dbc5b0b3379d5ab
David-mwas <dmwas704@gmail.com>
2025-08-18 16:44:16 +0300

    FIX(CORE): update bursary module, fee logic to be interoperable with library module, fix library borrow and return books

--- backend/routes/bursary.route.js
+++ backend/routes/bursary.route.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middleware/auth.middleware");
+
+// backend/routes/bursary.routes.js
 const {
+  recordFeePayment,
   getStudentFeeStatus,
   getTeacherEarnings,
-  recordFeePayment,
   recordTeacherPayment,
 } = require("../controllers/bursary.controller");
 
 // Record payment
-router.post("/fees/payment", authMiddleware, recordFeePayment);
+router.post("/fees/payment/:studentId", authMiddleware, recordFeePayment);
 
 // Get student's fee status & balance
 router.get("/fees/:studentId", authMiddleware, getStudentFeeStatus);
